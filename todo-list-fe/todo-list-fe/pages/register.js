eb7111ab05456aeb6013cd74b95c9614a2da802f
tiktaa <88105466+tiktaa@users.noreply.github.com>
2023-12-07 13:20:46 +0700

    init-register

---
+++ todo-list-fe/todo-list-fe/pages/register.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import styles from '../styles/globals.css'; 
+const Register = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setSuccess(false);
+    setError(null);
+
+    try {
+      const response = await fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const data = await response.json();
+      setSuccess(true);
+      setFormData({ name: '', email: '', password: '' });
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+
+        <button type="submit" disabled={loading}>
+          {loading ? 'Loading...' : 'Register'}
+        </button>
+      </form>
+      {success && <p>Registration successful!</p>}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+};
+
+export default Register;
