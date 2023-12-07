67d9f76a8128ed3bc8ec30dbd7fe53607c24598d
Hanifah Rahmajati <hanrahmajati@gmail.com>
2023-12-07 18:39:55 +0700

    login, todo (partial)

---
+++ todo-list-fe/pages/todo.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import '../styles/globalui.css';
+
+const TodoList = () => {
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [completed, setCompleted] = useState([]);
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        setTodos([...todos, input]);
+        setInput('');
+    };
+
+    const markAsCompleted = (index) => {
+        const updatedCompleted = [...completed, todos[index]];
+        const updatedTodos = todos.filter((_, i) => i !== index);
+        setCompleted(updatedCompleted);
+        setTodos(updatedTodos);
+    };
+
+    return (
+        <main class="container">
+            <h1 style={{ textAlign: 'center', paddingTop: '50px' }}>Todo List</h1>
+            <form onSubmit={addTodo}>
+                <input
+                    type="text"
+                    placeholder="Type your task here..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button type="submit">Add Task</button>
+            </form>
+            <div class="grid">
+                <h5>List</h5>
+                <h5>Completed</h5>
+            </div>
+            <div class="grid">
+                <ul>
+                    {todos.map((todo, index) => (
+                        <li key={index}>
+                            <input type="checkbox" onChange={() => markAsCompleted(index)} />
+                            {todo}
+                        </li>
+                    ))}
+                </ul>
+                <ul>
+                    {completed.map((todo, index) => (
+                        <li key={index} style={{ textDecoration: 'line-through' }}>
+                            {todo}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </main>
+    );
+};
+
+export default TodoList;
