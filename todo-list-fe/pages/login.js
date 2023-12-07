397dd344e2fa7738e5b6d70715c95e771cf725d1
Hanifah Rahmajati <hanrahmajati@gmail.com>
2023-12-07 18:40:42 +0700

    login, todo partial

--- todo-list-fe/pages/login.js
+++ todo-list-fe/pages/login.js
@@ -2,52 +2,39 @@ import React, { useState } from 'react';
 import '../styles/globalui.css';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+ const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Email:', email);
     console.log('Password:', password);
-  };
+ };
 
-  return (
-    <main class="container">
-      <section id="preview">
-        <h2>Preview</h2>
-        <p>
-          Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla
-          iaculis eros a elit pharetra egestas.
-        </p>
-        <form>
-          <div class="grid">
-            <input
-              type="text"
-              name="firstname"
-              placeholder="First name"
-              aria-label="First name"
-              required
-            />
+ return (
+    <main className="container" style={{ width: '30%' }}>
+        <h1 style={{ textAlign: 'center', paddingTop: '50px' }}>Login</h1>
+        <form onSubmit={handleSubmit}>
             <input
               type="email"
-              name="email"
-              placeholder="Email address"
-              aria-label="Email address"
+              id="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
-            <button type="submit">Subscribe</button>
-          </div>
-          <fieldset>
-            <label for="terms">
-              <input type="checkbox" role="switch" id="terms" name="terms" />
-              I agree to the
-              <a href="#" onclick="event.preventDefault()">Privacy Policy</a>
-            </label>
-          </fieldset>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit">Login</button>
         </form>
-      </section>
       </main>
-  );
+ );
 };
 
 export default LoginPage;
--- todo-list-fe/pages/reg.js
+++ todo-list-fe/pages/reg.js
@@ -3,53 +3,53 @@ import Link from 'next/link';
 import '../styles/globalui.css';
 
 const Register = () => {
- const [name, setName] = useState('');
- const [email, setEmail] = useState('');
- const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
- const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Submitted: ', { name, email, password });
- };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Submitted: ', { name, email, password });
+    };
 
- return (
-    <div className="container" style={{ width: '30%' }}>
-      <h1 style={{textAlign: 'center', paddingTop: '50px'}}>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="name"
-          placeholder='Name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+    return (
+        <div className="container" style={{ width: '30%' }}>
+            <h1 style={{ textAlign: 'center', paddingTop: '50px' }}>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder='Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
 
-       
-        <input
-          type="email"
-          id="email"
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
 
-       
-        <input
-          type="password"
-          id="password"
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+                <input
+                    type="email"
+                    id="email"
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
 
-        <button type="submit">Register</button>
-      </form>
 
-      <p>
-        Already have an account? <Link href="/login">Login</Link>
-      </p>
-    </div>
- );
+                <input
+                    type="password"
+                    id="password"
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit">Register</button>
+            </form>
+
+            <p>
+                Already have an account? <Link href="/login">Login</Link>
+            </p>
+        </div>
+    );
 };
 
 export default Register;
